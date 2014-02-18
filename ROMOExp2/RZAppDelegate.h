994224eea38562f7ea640abbc2ab272a90a19c65
ruiminz <ruiminz@mtu.edu>
2014-02-17 20:44:23 -0500

    Initial Commit

---
+++ ROMOExp2/RZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RZAppDelegate.h
+//  ROMOExp2
+//
+//  Created by RM on 2/17/14.
+//  Copyright (c) 2014 Ruimin Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ROMOExp2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ROMOExp2
+//
+//  Created by RM on 2/17/14.
+//  Copyright (c) 2014 Ruimin Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RZAppDelegate class]));
+    }
+}
